b8441719af5e07d72f8bf7e3e3ddb85c264ec568
ShiZhan <g.shizhan.g@gmail.com>
2014-06-01 17:45:36 -0500

    console just complicates the program design and prevents it from running in background

--- src/main/scala/kernel/Watcher.scala
+++ src/main/scala/kernel/Watcher.scala
@@ -8,28 +8,9 @@ object Watcher extends helper.Logging {
   private val httpValidator = new UrlValidator(httpSchemes)
   private var log = List[(Long, String)]((getMillis, "start"))
 
-  def console: Unit = {
-    val prompt = "> "
-    println("enter [exit] to exit, [tail] to show last 10 log entries.")
-    print(prompt)
-    for (line <- io.Source.stdin.getLines) {
-      val output = line.split(" ").toList match {
-        case "tail" :: Nil =>
-          log.takeRight(10)
-            .map { case (millis, md5) => millis.toDate + "\t" + md5 }
-            .mkString("Time                     \tChecksum\n---\n", "\n", "\n")
-        case "exit" :: Nil => return
-        case "" :: Nil => ""
-        case _ => "Unrecognized command: [%s]\n".format(line)
-      }
-      print(output + prompt)
-    }
-  }
-
   def run(target: String, cycle: Int, selector: String) = {
     if (httpValidator.isValid(target)) {
       logger.info("URL: [{}], cycle: [{}], selector: [{}]", target, cycle.toString, selector)
-      console
     } else logger.error("invalid URL: [{}]", target)
   }
 }
