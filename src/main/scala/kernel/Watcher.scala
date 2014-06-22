5853295eb7a22d7f12a46707a7c9f4ccfe7375b6
ShiZhan <g.shizhan.g@gmail.com>
2014-06-22 05:00:24 -0500

    BUG FIX: '-f' doesn't update downloaded page content

--- src/main/scala/kernel/Watcher.scala
+++ src/main/scala/kernel/Watcher.scala
@@ -8,27 +8,26 @@ object Watcher {
   private val logger = org.slf4j.LoggerFactory.getLogger(this.getClass)
 
   def run(target: String, cycle: Int, selector: String, diff: Boolean, full: Boolean): Unit = {
-    val timeout = 2000
-    var previous = ""
     logger.info("Options: [{}]", (target, cycle, selector, diff, full))
     logger.info("press Ctrl+C to exit")
+    val timeout = 2000
+    var previous = ""
     while (true) {
       try {
-        val dateTime = getMillis
         val doc = Jsoup.parse(new URL(target), timeout)
         val area = doc.select(selector)
         val current = area.toString
         if (current != previous) {
-          println(dateTime.toDate)
+          logger.info("Content changed")
           if (diff == true) {
             println("diff:")
             println(current diff previous)
-            previous = current
           }
           if (full == true) {
             println("content:")
             println(current)
           }
+          previous = current
         }
       } catch { case e: Exception => println(e) }
       Thread.sleep(cycle)
