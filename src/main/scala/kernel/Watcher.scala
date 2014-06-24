4e2e1f1480b03cb8b525cce4e18411d48d9fce50
ShiZhan <g.shizhan.g@gmail.com>
2014-06-23 21:00:09 -0500

    functional style

--- src/main/scala/kernel/Watcher.scala
+++ src/main/scala/kernel/Watcher.scala
@@ -10,26 +10,20 @@ object Watcher {
     logger.info("Options: [{}]", (target, cycle, selector, diff, full))
     logger.info("press Ctrl+C to exit")
     val timeout = 2000
-    var previous = ""
-    while (true) {
-      try {
-        val doc = Jsoup.parse(new URL(target), timeout)
-        val area = doc.select(selector)
-        val current = area.toString
-        if (current != previous) {
-          logger.info("Content changed")
-          if (diff == true) {
-            println("diff:")
-            println(current diff previous)
-          }
-          if (full == true) {
-            println("content:")
-            println(current)
-          }
-          previous = current
-        }
-      } catch { case e: Exception => println(e) }
-      Thread.sleep(cycle)
+    val url = new URL(target)
+    def check =
+      try { Some(Jsoup.parse(url, timeout).select(selector).toString) }
+      catch { case e: Exception => logger.error(e.toString); None }
+    val first = check.getOrElse(""); logger.info("Beginning content:"); println(first)
+    val samples = Iterator.continually { Thread.sleep(cycle); check }
+    (first /: samples) {
+      case (previous, Some(current)) if (current != previous) => {
+        logger.info("Content changed")
+        if (diff) println(current diff previous)
+        if (full) println(current)
+        current
+      }
+      case (previous, _) => previous
     }
   }
 }
